4ef609c138a85ca7cfc15865a05b6b36cedd4abe
tobyw4n <tobycollier@gmail.com>
2013-03-16 18:59:36 -0400

    clean up

--- puppet-lint-mercurial-hook.sh
+++ puppet-lint-mercurial-hook.sh
@@ -5,8 +5,8 @@
 which puppet-lint >/dev/null 2>&1
 LINTTRAP=$?
 if [ $LINTTRAP -ne 0 ]
-	then 
-	exit $LINTTRAP
+  then 
+  exit $LINTTRAP
 fi
 
 declare -a FILES
@@ -15,15 +15,15 @@ FILES=$(hg status -man)
 
 for file in ${FILES[@]}
 do
-	if [[ $file =~ \.*.pp$ ]]
-		then
-		puppet-lint --with-filename "$file"
-		RC=$?
-		if [ $RC -ne 0 ]
-			then
-			exit $RC
-		fi
-	fi
+  if [[ $file =~ \.*.pp$ ]]
+    then
+    puppet-lint --with-filename "$file"
+    RC=$?
+    if [ $RC -ne 0 ]
+      then
+      exit $RC
+    fi
+  fi
 done
 
 exit 0
